4621bb4839515099ff37f788fe201dd8867eccb3
Viet Nguyen Tran <vietstone.ng@gmail.com>
2024-03-12 11:06:21 +0700

    add methods

--- lib/apple_in_app_receipt.dart
+++ lib/apple_in_app_receipt.dart
@@ -8,4 +8,16 @@ class AppleInAppReceipt {
   Future<bool> verifySubscription(String productId) {
     return AppleInAppReceiptPlatform.instance.verifySubscription(productId);
   }
+
+  Future<bool> verifyPurchase(String productId) {
+    return AppleInAppReceiptPlatform.instance.verifyPurchase(productId);
+  }
+
+  Future<bool> haveActiveSubscription() {
+    return AppleInAppReceiptPlatform.instance.haveActiveSubscription();
+  }
+
+  Future<bool> havePurchases() {
+    return AppleInAppReceiptPlatform.instance.havePurchases();
+  }
 }
